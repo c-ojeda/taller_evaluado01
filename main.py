290e7f31a2a44b74b9993fea6941fae672bc27a4
Gety Flores <getyflores@gmail.com>
2026-08-28 12:37:01 -0400

    Endpoints de inicio, usuarios y productos finalizados

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+
 app = FastAPI()
+
 @app.get("/")
 def inicio():
     return {
@@ -7,4 +9,22 @@ def inicio():
         "statusCode": 200,
         "message": "API de Taller Evaluado 1 funcionando correctamente",
         "data": None
+    }
+
+@app.get("/usuarios")
+def obtener_usuarios():
+    return {
+        "success": True,
+        "statusCode": 200,
+        "message": "Lista de usuarios obtenida con éxito",
+        "data": ["pedro", "maria", "fernanda"]
+    }
+
+@app.get("/productos")
+def obtener_productos():
+    return {
+        "success": True,
+        "statusCode": 200,
+        "message": "Productos encontrados",
+        "data": [{"id": 1, "nombre": "Notebook"}]
     }
